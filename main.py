a17c7fa25aec520f02ad3a887c38c9b964d126b8
EkSVaY <v.ekin@g.nsu.ru>
2024-03-12 19:14:34 +0700

    Create1

---
+++ main.py
@@ -0,0 +1,21 @@
+# Case-study #3
+# Developers: Ekin Viacheslaw, Amat Tolkochokov, Ilya Sokolov, Askar Gazizov
+#
+import ru_local as ru
+
+MAX_MONTH = 12
+
+
+def free_tax():
+    '''
+    The function determines the annual tax-free amount.
+    '''
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.TEXT_FREE_TAX} {ru.NAME[month]} [USD]: '))
+        amount += value
+    return amount
+
+
+if __name__ == '__main__':
+    main()
